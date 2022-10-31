befde49c71d126e1229fd2025ce055582fa51532
blaze21777 <stephen.411@hotmail.co.uk>
2022-10-31 13:31:53 +0000

    Filter now only shows selected fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,9 @@ streamlit.text("🥑🍞 Avocado toast")
 
 streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
 # Let's put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect(
+
+fruits_selected = streamlit.multiselect(
     "Pick some fruits: ", list(my_fruit_list.index), ["Avocado", "Strawberries"]
 )
-streamlit.dataframe(my_fruit_list)
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
