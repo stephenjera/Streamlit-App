3a4dccbac4f06fac1670ee68d72ddef2fd69ace9
blaze21777 <stephen.411@hotmail.co.uk>
2022-11-01 14:27:52 +0000

    Fixed error with converting to json

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,4 +27,4 @@ streamlit.dataframe(fruits_to_show)
 # NEw section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response).json()
+streamlit.text(fruityvice_response.json())
