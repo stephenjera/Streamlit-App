47c453218ff66dd752c3b833853700542090de42
blaze21777 <stephen.411@hotmail.co.uk>
2022-11-01 14:35:59 +0000

    Add fruit choice to api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,13 @@ streamlit.dataframe(fruits_to_show)
 
 # NEw section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruit_choice = streamlit.text_input(
+    "What fruit would you like information about?", "Kiwi"
+)
+streamlit.write("The user entered ", fruit_choice)
+
+fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
+# streamlit.text(fruityvice_response.json())
 
 # Normalise json data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
