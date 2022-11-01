ec7e26a499b52f1d1c545ce4029ea07fa1a917a4
blaze21777 <stephen.411@hotmail.co.uk>
2022-11-01 14:31:47 +0000

    Print json as table

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,3 +28,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Normalise json data
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Output json as table
+streamlit.dataframe(fruityvice_normalized)
