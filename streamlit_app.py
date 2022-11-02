ee190352a03258e7d1bcada3cb03a4bc6873e6f1
blaze21777 <stephen.411@hotmail.co.uk>
2022-11-02 03:31:26 +0000

    Changed new textbox entry text

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,6 +50,6 @@ streamlit.dataframe(my_data_rows)
 
 # Allow user to add fruit to list 
 add_my_fruit = streamlit.text_input(
-    "What fruit would you like information about?", "Kiwi"
+    "What fruit would you like to add?", "Jackfruit"
 )
 streamlit.write("Thanks for adding ", add_my_fruit)
